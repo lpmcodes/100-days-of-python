22a2c2036cea5c46446a1190d74e3cdf714f566b
Lucas <lucas.paivamagalhaes@gmail.com>
2026-03-29 22:04:04 -0300

    second challenge

--- day1.py
+++ day1.py
@@ -18,9 +18,21 @@ print (lenght)
 print(len(input('What is your name?\n')))
 '''
 
+'''
 # challenge
 username = input('What is your name?\n')
 lenght = len(username)
 print('The lenght of username word is: ' + str(lenght)) # you can turn into a string using 'str()'
 print('The lenght of username word is:', lenght) # or using ',' -> don't need space after the text
 print(f'The lenght of username word is: {lenght}') # or using "f-string"
+'''
+
+# challenge 2 (Write 3 lines of code to switch the contents of the variables)
+glass1 = 'milk'
+glass2 = 'juice'
+
+aux = glass1 # 'aux' is a temporary variable to help you
+glass1 = glass2
+glass2 = aux
+
+print(glass1 + '\n' + glass2)

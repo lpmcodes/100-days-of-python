075c70e8224bd9f3fefe20b852fd859a00dba5a7
Lucas <lucas.paivamagalhaes@gmail.com>
2026-03-29 21:44:37 -0300

    'len()'

--- day1.py
+++ day1.py
@@ -9,5 +9,18 @@ print("Hello\nWorld!")
 # name = input('What is your name?\n')
 # print(name)
 
+'''
 name = 'Lucas'
 print(name)
+lenght = len(name) # with 'len()' you can find out the lenght of your word
+print (lenght)
+
+print(len(input('What is your name?\n')))
+'''
+
+# challenge
+username = input('What is your name?\n')
+lenght = len(username)
+print('The lenght of username word is: ' + str(lenght)) # you can turn into a string using 'str()'
+print('The lenght of username word is:', lenght) # or using ',' -> don't need space after the text
+print(f'The lenght of username word is: {lenght}') # or using "f-string"

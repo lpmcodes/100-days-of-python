f9765918fe493bc1ef69c678b70ab735176b09e5
Lucas <lucas.paivamagalhaes@gmail.com>
2026-03-29 21:22:50 -0300

    first commit

---
+++ day1.py
@@ -0,0 +1,13 @@
+'''
+print("Hello World")
+print("Hello" + " Angela")
+print("Hello\nWorld!")
+'''
+
+# print('Hello ' + input('What is your name?\n') + '!')
+
+# name = input('What is your name?\n')
+# print(name)
+
+name = 'Lucas'
+print(name)

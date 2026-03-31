ce4c619666fee8a849e2a7c6a916925c94091be1
Lucas <lucas.paivamagalhaes@gmail.com>
2026-03-30 21:01:30 -0300

    simple fix challenge

--- day1_to_day10/day2.py
+++ day1_to_day10/day2.py
@@ -1,5 +1,9 @@
 # Use 'type' to find out what data type you are using
+'''
 print(type('123'))
 print(type(123))
 print(type(123.5))
 print(type(True))
+'''
+# challenge (make this line of code run without errors)
+print('Number of letters in your name:', len(input('Enter yout name: ')))

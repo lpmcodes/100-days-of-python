527102e7a0edd7b4b63034b31a0c3ec07cf70bb1
Lucas <lucas.paivamagalhaes@gmail.com>
2026-03-30 21:24:29 -0300

    math operations

--- day1_to_day10/day2.py
+++ day1_to_day10/day2.py
@@ -6,4 +6,21 @@ print(type(123.5))
 print(type(True))
 '''
 # challenge (make this line of code run without errors)
+'''
 print('Number of letters in your name:', len(input('Enter yout name: ')))
+'''
+# math operations
+'''
+print(5 / 3)
+print(5 // 3) # you can get an integer result by using '//'
+
+print(5 ** 2) # by using '**', you can raise a number to a power
+'''
+
+# priority order
+# PEMDAS - PARANTHESES, EXPONENTS, MULTIPLICATION/DIVISION, ADDITION/SUBTRACTION
+# ()
+# **
+# * OR (/ -> //)
+# + OR -
+print((int(3 * (3 + 3) / 3 - 3)))

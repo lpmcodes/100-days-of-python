968159d6af40d3255050a9d960802f97a5e7abae
Lucas <lucas.paivamagalhaes@gmail.com>
2026-04-06 23:12:38 -0300

    if /else

---
+++ day1_to_day10/day3.py
@@ -0,0 +1,14 @@
+# ----------------- // -----------------
+# conditional statements
+# logical operators
+# code blocks
+# scope
+# ----------------- // -----------------
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can buy a ticket!")
+else:
+    print("You can't buy a ticket!")

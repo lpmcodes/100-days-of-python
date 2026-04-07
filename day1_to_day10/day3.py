962394e776b0e49192af393b9027af25984afa27
Lucas <lucas.paivamagalhaes@gmail.com>
2026-04-06 23:45:20 -0300

    if inside of another if

--- day1_to_day10/day3.py
+++ day1_to_day10/day3.py
@@ -20,7 +20,7 @@ print(10 % 3)
 '''
 
 # check if a number is even or odd
-
+'''
 try:
     number = int(input("Type a number: "))
 
@@ -32,4 +32,20 @@ if number % 2 == 0:
     print(f"{number} is even.")
 else:
     print(f"{number} is odd.")
-    
+'''
+# ----------------- // -----------------
+# if inside of another if
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can buy a ticket!")
+    age = int(input("What is your age? "))
+    if age >= 18:
+        print("You have to pay 10 $.")
+    elif age > 12 and age < 18:
+        print("You have to pay 7 $.")
+    else:
+        print("You have to pay 5 $.")
+else:
+    print("You can't buy a ticket!")

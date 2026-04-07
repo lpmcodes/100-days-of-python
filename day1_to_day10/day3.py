d3546eeacaba1af8cde9bb5502136149c8f7f87c
Lucas <lucas.paivamagalhaes@gmail.com>
2026-04-06 23:35:16 -0300

    modulo operator

--- day1_to_day10/day3.py
+++ day1_to_day10/day3.py
@@ -5,6 +5,7 @@
 # scope
 # ----------------- // -----------------
 
+'''
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 
@@ -12,3 +13,23 @@ if height >= 120:
     print("You can buy a ticket!")
 else:
     print("You can't buy a ticket!")
+'''
+# shows the rest of a division
+'''
+print(10 % 3)
+'''
+
+# check if a number is even or odd
+
+try:
+    number = int(input("Type a number: "))
+
+except ValueError:
+    print("That's not a number. Try again!")
+    exit()
+    
+if number % 2 == 0:
+    print(f"{number} is even.")
+else:
+    print(f"{number} is odd.")
+    
